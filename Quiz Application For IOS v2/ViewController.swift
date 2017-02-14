4df3c9ccfb95128039614ad787e8f105654e9537
CasiKea <casianoros1223@gmail.com>
2017-02-14 21:38:29 +0100

    Initial Commit

---
+++ Quiz Application For IOS v2/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  Quiz Application For IOS v2
+//
+//  Created by eak on 14/02/2017.
+//  Copyright © 2017 eak. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
